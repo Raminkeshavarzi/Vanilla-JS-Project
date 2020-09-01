451602887c943446b84a910f571542e3f28b1a9c
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-01 09:07:04 +0200

    added the logic of checking winner of each game

--- DOM-exercise/App.js
+++ DOM-exercise/App.js
@@ -84,3 +84,34 @@ const warriorsGames = [
 		}
 	}
 ];
+
+// the logic of checking and appending it into the HTML's file
+
+const makeChart = (games, targetTeam) => {
+	const ulParent = document.createElement('ul');
+	for (let game of games) {
+		const gameLi = document.createElement('li');
+		gameLi.innerHTML = getScoreLine(game);
+		gameLi.classList.add(isWinner(game, targetTeam) ? 'win' : 'loss');
+		ulParent.appendChild(gameLi);
+	}
+	return ulParent;
+};
+
+const isWinner = ({ homeTeam, awayTeam }, targetTeam) => {
+	const target = homeTeam.team === targetTeam ? homeTeam : awayTeam;
+	return target.isWinner;
+};
+
+const getScoreLine = ({ homeTeam, awayTeam }) => {
+	const { team: hTeam, points: hPoints } = homeTeam;
+	const { team: aTeam, points: aPoints } = awayTeam;
+	const teamNames = `${aTeam} @ ${hTeam}`;
+	let scoreLine;
+	if (aPoints > hPoints) {
+		scoreLine = `<b>${aPoints}</b>-${hPoints}`;
+	} else {
+		scoreLine = `${aPoints}-<b>${hPoints}</b>`;
+	}
+	return `${teamNames} ${scoreLine}`;
+};

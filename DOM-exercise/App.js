b8d3c7b4545331c0814619d690798147e659a0f6
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-01 09:05:41 +0200

    added the games info

---
+++ DOM-exercise/App.js
@@ -0,0 +1,86 @@
+const warriorsGames = [
+	{
+		away: {
+			team: 'Golden State',
+			points: 119,
+			isWinner: true
+		},
+		home: {
+			team: 'Houston',
+			points: 106,
+			isWinner: false
+		}
+	},
+	{
+		away: {
+			team: 'Golden State',
+			points: 105,
+			isWinner: false
+		},
+		home: {
+			team: 'Houston',
+			points: 127,
+			isWinner: true
+		}
+	},
+	{
+		home: {
+			team: 'Golden State',
+			points: 126,
+			isWinner: true
+		},
+		away: {
+			team: 'Houston',
+			points: 85,
+			isWinner: false
+		}
+	},
+	{
+		home: {
+			team: 'Golden State',
+			points: 92,
+			isWinner: false
+		},
+		away: {
+			team: 'Houston',
+			points: 95,
+			isWinner: true
+		}
+	},
+	{
+		away: {
+			team: 'Golden State',
+			points: 94,
+			isWinner: false
+		},
+		home: {
+			team: 'Houston',
+			points: 98,
+			isWinner: true
+		}
+	},
+	{
+		home: {
+			team: 'Golden State',
+			points: 115,
+			isWinner: true
+		},
+		away: {
+			team: 'Houston',
+			points: 86,
+			isWinner: false
+		}
+	},
+	{
+		away: {
+			team: 'Golden State',
+			points: 101,
+			isWinner: true
+		},
+		home: {
+			team: 'Houston',
+			points: 92,
+			isWinner: false
+		}
+	}
+];
